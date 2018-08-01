02a355ce1326af587801bd90e4f5064b40bf63d3
Egor Vasilyev <egorvas@gmail.com>
2018-08-01 16:40:47 +0700

    Update to debug version

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-IMAGE_NAME=${1:-"jshimko/meteor-launchpad"}
+IMAGE_NAME=${1:-"everex/launchpad"}
 
 printf "\n[-] Building $IMAGE_NAME...\n\n"
 
 docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
-docker build -t $IMAGE_NAME:latest .
+docker build -t $IMAGE_NAME:1.7 .
