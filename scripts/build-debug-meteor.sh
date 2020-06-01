e69a13dd2843477d1dfb4b4ae811e8ea72ef670f
Egor Vasilyev <egorvas@gmail.com>
2020-06-01 14:05:29 +0300

    Revert "fix folders"
    This reverts commit febff7f7b83234f58fb8b9e591487125c27d27ae.

--- scripts/build-debug-meteor.sh
+++ scripts/build-debug-meteor.sh
@@ -9,12 +9,15 @@ set -e
 # earlier versions of Meteor with --unsafe-perm or --allow-superuser
 # https://github.com/meteor/meteor/issues/7959
 export METEOR_ALLOW_SUPERUSER=true
+
+cp -r $NPM_DIRECTORY/node_modules $APP_SOURCE_DIR/node_modules
+cp  $NPM_DIRECTORY/package-lock.json $APP_SOURCE_DIR/package-lock.json
 cd $APP_SOURCE_DIR
 
 # Install app deps
 printf "\n[-] Running npm install in app directory...\n\n"
-meteor cache verify --verbose
-meteor install --debug --verbose
+npm cache verify
+npm install --debug
 
 # build the bundle
 printf "\n[-] Building Meteor application...\n\n"
@@ -24,8 +27,8 @@ meteor build --debug --directory $APP_BUNDLE_DIR --server-only --verbose
 # run npm install in bundle
 printf "\n[-] Running npm install in the server bundle...\n\n"
 cd $APP_BUNDLE_DIR/bundle/programs/server/
-meteor npm cache verify --verbose
-meteor npm install --debug --verbose
+meteor npm cache verify
+meteor npm install --debug
 
 # put the entrypoint script in WORKDIR
 mv $BUILD_SCRIPTS_DIR/entrypoint.sh $APP_BUNDLE_DIR/bundle/entrypoint.sh
